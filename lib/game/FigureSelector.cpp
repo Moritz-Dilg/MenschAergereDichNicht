5dd554469f95487c104edc4a9e4de0ed1b809673
Felix <40723033+DerFelixPlayer2@users.noreply.github.com>
2022-10-21 10:43:09 +0200

    Add figure selector structure

---
+++ lib/game/FigureSelector.cpp
@@ -0,0 +1,5 @@
+#include "FigureSelector.hpp"
+
+short FigureSelector::select(LED* led, short positions[4]) {
+	// TODO: implement
+}
---
+++ lib/game/FigureSelector.hpp
@@ -0,0 +1,18 @@
+#include "LED.hpp"
+
+class FigureSelector {
+   public:
+	/**
+	 * @brief The button pressed by the user. Updated by an interrupt routine.
+	 */
+	static u_int8_t pressed_button;
+
+	/**
+	 * @brief Prompts the user for a figure to select. Syncronous function.
+	 * @param led The LED instance
+	 * @param positions The positions of the figures
+	 *
+	 * @returns The current position of the selected figure
+	 */
+	static short select(LED* led, short positions[4]);
+};
