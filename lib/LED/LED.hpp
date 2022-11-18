68789f0db0249e5d1711e6d08e80cddb4aa06326
Moritz-Dilg <moritz@dilg-online.de>
2022-11-18 13:12:18 +0100

    Finished PCB

--- lib/LED/LED.hpp
+++ lib/LED/LED.hpp
@@ -1,33 +1,27 @@
-#define LED_PIN_FIELD 33
-#define LED_PIN_GOAL 31
-#define LED_PIN_BASE 26  // 30
-
 #include <Adafruit_NeoPixel.h>
 
 #include "../types.hpp"
 
 class LED {
    public:
-    LED(u_int8_t brightness);
-    ~LED();
+	LED(u_int8_t brightness);
+	~LED();
 
-    void begin();
-    void setBase(u_int8_t n, u_int8_t color);
-    void setGoal(u_int8_t n, u_int8_t color, int8_t from = -1);
-    void setFigureToStart(u_int8_t color);
-    void removeFigureFromField(u_int8_t position);
-    void moveFigure(u_int8_t i, u_int8_t n, u_int8_t color);
+	void begin();
+	void setBase(u_int8_t n, u_int8_t color);
+	void setGoal(u_int8_t n, u_int8_t color, int8_t from = -1);
+	void setFigureToStart(u_int8_t color);
+	void removeFigureFromField(u_int8_t position);
+	void moveFigure(u_int8_t i, u_int8_t n, u_int8_t color);
 
    private:
-    bool verifyNFigures(u_int8_t n) {
-        return n >= 0 && n <= 4;
-    }
-    void clearSection(u_int8_t index, Adafruit_NeoPixel &strip) {
-        for (u_int8_t i = index * 4; i < index * 4 + 4; i++) {
-            strip.setPixelColor(i, strip.Color(0, 0, 0));
-        }
-    }
+	bool verifyNFigures(u_int8_t n) { return n >= 0 && n <= 4; }
+	void clearSection(u_int8_t index, Adafruit_NeoPixel &strip) {
+		for (u_int8_t i = index * 4; i < index * 4 + 4; i++) {
+			strip.setPixelColor(i, strip.Color(0, 0, 0));
+		}
+	}
 
    protected:
-    u_int8_t brightness;
+	u_int8_t brightness;
 };
--- lib/types.hpp
+++ lib/types.hpp
@@ -11,8 +11,12 @@
 #define LED_BRIGHTNESS 50			   // Brightness of the LEDs: 1 - 255
 #define FIGURE_SELECT_TOGGLE_TIME 500  // Time in ms to toggle the selected LED
 
-// TODO: Set correct pins
+// LED Pins
+#define LED_PIN_FIELD 33
+#define LED_PIN_GOAL 25
+#define LED_PIN_BASE 32
+
 // Button Pins
-#define PIN_BTN_1 0
-#define PIN_BTN_2 1
-#define PIN_BTN_3 2
+#define PIN_BTN_1 14
+#define PIN_BTN_2 27
+#define PIN_BTN_3 26
