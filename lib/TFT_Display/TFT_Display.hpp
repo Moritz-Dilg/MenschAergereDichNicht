134f90c0e1cad4d35ee4a403b023930d3a0f5765
Moritz-Dilg <moritz@dilg-online.de>
2023-03-10 10:09:41 +0100

    Added functions to set text manually

--- lib/TFT_Display/TFT_Display.hpp
+++ lib/TFT_Display/TFT_Display.hpp
@@ -2,12 +2,20 @@
 
 #include "../types.hpp"
 
+struct TextLine {
+	const char* text;
+	u_int8_t size;
+};
+
 class TFT_Display {
    public:
 	TFT_Display(short brightness);
 	~TFT_Display();
 
 	void showSettings();
+	void setTextTop(const char* text);
+	void setTextCenterLeft(TextLine text[], u_int8_t nLines);
+	void setTextCenterRight(TextLine text[], u_int8_t nLines);
 	void setCurrentPlayer(u_int8_t player);
 	void rollDice(u_int8_t result);
 	void setNextInstruction(const char* instruction);
@@ -16,4 +24,6 @@ class TFT_Display {
 
    private:
 	void drawLines();
+	u_int8_t printLine(TextLine line, u_int8_t startX, u_int8_t stopX,
+					   u_int8_t y);
 };
