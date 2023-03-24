5087e5683f479782a391c151229ce4c284b81be4
Moritz-Dilg <moritz@dilg-online.de>
2023-03-24 09:23:42 +0100

    Update TFT at change of setting

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,7 @@ LED_CONTROLLER *led;
 Game *game;
 
 void playGame(u_int8_t player_count);
-int changeSetting(u_int8_t min, u_int8_t max, u_int8_t inc,
-				  u_int8_t defaultValue);
+int changeSetting(u_int8_t min, u_int8_t max, u_int8_t inc, u_int8_t defaultValue, std::function<void(u_int8_t)> updateTFT);
 
 void setup(void) {
 	tft = new TFT_Display();
@@ -59,17 +58,12 @@ void loop() {
 					}
 				} else if (button == BTN_C) {
 					if (selected == 0) {
-						tft->setButton(BTN_A, "-");
-						tft->setButton(BTN_B, "+");
-						tft->setButton(BTN_C, "OK");
-						brightness =
-							changeSetting(0, 100, 10, brightness / 2.5) * 2.5;
+						auto updateTFT = [&](u_int8_t value) { return tft->showSettings(value, player_count, selected); };
+						brightness = changeSetting(0, 100, 10, brightness / 2.5, updateTFT) * 2.5;
 						led->setBrightness(brightness);
 					} else if (selected == 1) {
-						tft->setButton(BTN_A, "-");
-						tft->setButton(BTN_B, "+");
-						tft->setButton(BTN_C, "OK");
-						player_count = changeSetting(2, 4, 1, player_count);
+						auto updateTFT = [&](u_int8_t value) { return tft->showSettings(brightness / 2.5, value, selected); };
+						player_count = changeSetting(2, 4, 1, player_count, updateTFT);
 					} else if (selected == 2) {
 						break;
 					}
@@ -94,18 +88,22 @@ void loop() {
 	}
 }
 
-int changeSetting(u_int8_t min, u_int8_t max, u_int8_t inc,
-				  u_int8_t defaultValue) {
+int changeSetting(u_int8_t min, u_int8_t max, u_int8_t inc, u_int8_t defaultValue, std::function<void(u_int8_t)> updateTFT) {
+	tft->setButton(BTN_A, "-");
+	tft->setButton(BTN_B, "+");
+	tft->setButton(BTN_C, "OK");
 	u_int8_t value = defaultValue;
 	while (true) {
 		u_int8_t button = Buttons::getPressedButton();
 		if (button == BTN_A) {
 			if (value - inc >= min) {
 				value -= inc;
+				updateTFT(value);
 			}
 		} else if (button == BTN_B) {
 			if (value + inc <= max) {
 				value += inc;
+				updateTFT(value);
 			}
 		} else if (button == BTN_C) {
 			return value;
